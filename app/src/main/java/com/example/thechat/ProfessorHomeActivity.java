e3dbf366c49bf16a5fd55e806db608607b3aa57b
Matheuswrs <Matheuswrs@gitlab.com>
2019-11-14 00:04:02 -0300

    adicionando botão sair tela Professor e nome botão da tela aluno

--- app/src/main/java/com/example/thechat/ProfessorHomeActivity.java
+++ app/src/main/java/com/example/thechat/ProfessorHomeActivity.java
@@ -1,7 +1,12 @@
 package com.example.thechat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfessorHomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class ProfessorHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
+
+        Button logout = (Button) findViewById(R.id.idSairProfessor);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(ProfessorHomeActivity.this, MainActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
